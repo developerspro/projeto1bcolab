b76ac692ea7beaf433b4331e3589d85457c4785f
Kelvinho423 <107007661+Kelvinho423@users.noreply.github.com>
2022-06-28 22:05:29 -0300

    Add files via upload

---
+++ projeto1bcolab/alunos1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            alunos aluno;
+            aluno = new Pessoa();
+            alunos professor = new Pessoa();
+            aluno.nome = "kelvinho";
+            professor.nome = "pardal";
+            Console.WriteLine("aluno:" + aluno.nome);
+            Console.Writeline("professor:" + professor.nome);
+            Console.ReadKey();
+            
+    }
+}
