138d8ae0606f13cf6af91d748f85228e8c8eede1
Arthur Lucas <arthur.silva327@etec.sp.gov.br>
2022-06-30 22:27:48 -0300

    teste

---
+++ Classes PCs Arthur/Eletronico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_PCs_Arthur
+{
+    class Eletronico
+    {
+        public string modelo;
+        public double serie;
+        public double valor;
+        public string tipo;
+    }
+}
---
+++ Classes PCs Arthur/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_PCs_Arthur
+{
+    class Eletronico
+    {
+        static void Main(string[] args)
+        {
+            //Arthur
+            Eletronico PC = new Eletronico();
+            PC.tipo = "PC gamer";
+            PC.modelo = "Acer nitro 5x";
+            PC.valor = double.Parse("12000");
+            PC.serie = double.Parse("894564564");
+            Console.WriteLine("tipo: " + PC.tipo);
+            Console.WriteLine("valor: R$" + PC.valor);
+            Console.WriteLine("serie: " + PC.serie);
+            Console.WriteLine("modelo: " + PC.modelo);
+
+            Console.ReadKey();
+        }
+    }
+}
